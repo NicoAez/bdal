112b2a4724f36cc0e2d240edf727a41ecb0da797
Nico <nico.koeckerbauer@gmx.de>
2024-04-23 10:29:15 +0200

    finish lab one

--- src/lab_one/Tabulation.java
+++ src/lab_one/Tabulation.java
@@ -1,6 +1,7 @@
 package lab_one;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Tabulation {
@@ -9,8 +10,10 @@ public class Tabulation {
         var b_table = generateTable(true);
         ArrayList<Double> norm = new ArrayList<>();
         ArrayList<Double> mixed = new ArrayList<>();
+        int[] countAmountFlippedNorm = new int[32];
+        int[] countAmountFlippedMixed = new int[32];
 
-        for(int i = 0; i < 10000000; i++) {
+        for(int i = 0; i < 10_000_000; i++) {
             double normFlips = 0;
             double mixedFlips = 0;
 
@@ -19,7 +22,9 @@ public class Tabulation {
             for(int j = 0; j < 32; j++) {
                 int shifted = tab32 ^ (1<<j);
                 int tab32_x = (int) tabula32(shifted, s_table);
-                normFlips += Integer.bitCount(tab32 ^ tab32_x);
+                int amountFlipped = Integer.bitCount(tab32 ^ tab32_x);
+                countAmountFlippedNorm[amountFlipped]++;
+                normFlips += amountFlipped;
             }
 
             // mixed
@@ -27,20 +32,28 @@ public class Tabulation {
             for(int j = 0; j < 32; j++) {
                 int shifted = mix32 ^ (1<<j);
                 int mix32_x = (int) tabulaMix32(shifted, s_table, b_table);
-                mixedFlips += Integer.bitCount(mix32 ^ mix32_x);
+                int amountFlipped = Integer.bitCount(mix32 ^ mix32_x);
+                countAmountFlippedMixed[amountFlipped]++;
+                mixedFlips += amountFlipped;
             }
 
+            // add to list
             norm.add(normFlips / 32);
             mixed.add(mixedFlips / 32);
         }
 
+        // print avgs
         double normAvg = norm.stream().mapToDouble(a -> a).average().getAsDouble();
-        //double mixedAvg = mixed.stream().mapToDouble(a -> a).average().getAsDouble();
-        System.out.println(normAvg);
-        //System.out.println(mixedAvg);
+        double mixedAvg = mixed.stream().mapToDouble(a -> a).average().getAsDouble();
+        System.out.println("Norm: " + normAvg);
+        System.out.println("Mixed: " + mixedAvg);
+
+        // print chi
+        System.out.println("Norm: " + computeChiSquared(countAmountFlippedNorm));
+        System.out.println("Mixed: " + computeChiSquared(countAmountFlippedMixed));
     }
 
-    public static long tabula32(long x, long[][] hTab) {
+    static long tabula32(long x, long[][] hTab) {
         long hash = 0;
         long c;
 
@@ -53,24 +66,24 @@ public class Tabulation {
         return hash & 0xffffffffL;
     }
 
-    public static long tabulaMix32(long x, long[][] hTab_32, long[][] hTab_64) {
+    static long tabulaMix32(long x, long[][] hTab_32, long[][] hTab_64) {
         long hash = 0;
 
         for(int i = 0; i < 4; i++) {
-            hash ^= hTab_64[i][(byte) x];
+            hash ^= hTab_64[i][(int) (x & 0xff)];
             x = x >> 8;
         }
 
         long drv = hash >> 32;
         for(int i = 0; i < 4; i++){
-            hash ^= hTab_32[i][(byte) drv];
+            hash ^= hTab_32[i][(int) (drv & 0xff)];
             drv = drv >> 8;
         }
 
         return hash & 0xffffffffL;
     }
 
-    public static long[][] generateTable(boolean big) {
+    static long[][] generateTable(boolean big) {
         Random r = new Random();
         long max = big ? Long.MAX_VALUE : Integer.MAX_VALUE;
         long[][] table = new long[4][256];
@@ -82,5 +95,29 @@ public class Tabulation {
         }
 
         return table;
+    }
+
+    static double computeChiSquared(int[] observed) {
+        double result = 0;
+        int mult = 10000000 * 32;
+        for(int i = 0; i < 32; i++) {
+            double expected = binomialInt(32, i) * Math.pow(0.5, 32) * mult;
+
+            //System.out.println("Expected " + expected + ", got " + observed[i]);
+
+            result += Math.pow(observed[i] - expected, 2.0) / expected;
+        }
+
+        return result;
+    }
+
+    static long binomialInt(int n, int k) {
+        if (k > n - k)
+            k = n - k;
+
+        long binom = 1;
+        for (int i = 1; i <= k; i++)
+            binom = binom * (n + 1 - i) / i;
+        return binom;
     }
 }
